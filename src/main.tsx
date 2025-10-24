0005daee2158a0a4f728dd2fde63a80f20c51dc7
Simon Villar <sim.villar@duocuc.cl>
2025-10-23 23:45:00 -0300

    Se configura la estructura de carpetas inicial para migracion de proyecto a React+vite con Typescript. Se borran archivos innecesarios y se ajusta el main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import MyAwesomeApp from './MyAwesomeApp'
+import MyAwesomeApp from './MyApp'
 import './styles.css'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
